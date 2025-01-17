d0f0f9e61d47d7c3fc7a9e2f2c52f0b19d20e81f
nguyenhuyttns <huy2010200320102003@gmail.com>
2025-01-17 15:25:51 +0700

    call api add todo into mongdb

--- UIFlutter/todoapp/lib/config.dart
+++ UIFlutter/todoapp/lib/config.dart
@@ -1,3 +1,4 @@
 final url = 'http://10.15.17.141:3000/';
 final registration = url + "registration";
 final login = url + "login";
+final addtodo = url + "storeTodo";
--- UIFlutter/todoapp/lib/dashboard.dart
+++ UIFlutter/todoapp/lib/dashboard.dart
@@ -1,24 +1,55 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:http/http.dart' as http;
+import 'config.dart';
 
 class Dashboard extends StatefulWidget {
-  final token;
-  const Dashboard({@required this.token, Key? key}) : super(key: key);
+  final String token;
+  const Dashboard({required this.token, Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  late String email;
+  late String userId;
+
+  final TextEditingController _todoTitle = TextEditingController();
+  final TextEditingController _todoDesc = TextEditingController();
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(widget.token);
 
-    email = jwtDecodedToken['email'];
+    userId = jwtDecodedToken['_id'];
+  }
+
+  void addToDo() async {
+    if (_todoTitle.text.isNotEmpty && _todoDesc.text.isNotEmpty) {
+      var regBody = {
+        "userId": userId,
+        "title": _todoTitle.text,
+        "desc": _todoDesc.text
+      };
+
+      var response = await http.post(Uri.parse(addtodo),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(regBody));
+
+      var jsonResponse = jsonDecode(response.body);
+      print(jsonResponse['status']);
+
+      if (jsonResponse['status']) {
+        _todoDesc.clear();
+        _todoTitle.clear();
+        Navigator.pop(context);
+      } else {
+        print('Something wrong');
+      }
+    }
   }
 
   @override
@@ -28,10 +59,67 @@ class _DashboardState extends State<Dashboard> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(email),
+            Text(userId),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayTextInputDialog(context),
+        child: const Icon(Icons.add),
+        tooltip: 'App-ToDo',
+      ),
+    );
+  }
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add To-Do'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _todoTitle,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Title",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _todoDesc,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Description",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  addToDo();
+                },
+                child: const Text("Add"),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
